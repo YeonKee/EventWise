d5c0a76c9fbc36053122d1753cd34b70de351f94
YeonKee <yeon101702@gmail.com>
2023-10-26 00:29:59 +0800

    testingClone

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,6 @@
 
 {{-- 4154f1 --}}
 
-<a href='/staffs/dashboard'>Dashboard</a>
+<a href='/staffs/dashboard'>Dashboardaaa</a>
 <a href="/staffs/students/viewStudent" class="btn btn-primary">Student</a>
 <a href="/students/create" class="btn btn-primary">Student</a>
