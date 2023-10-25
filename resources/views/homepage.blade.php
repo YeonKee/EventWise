7a5c04b6d22c730dd99455427f98ed64ef1c14b7
YeonKee <yeon101702@gmail.com>
2023-10-26 00:38:16 +0800

    testing add

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,6 @@
 
 {{-- 4154f1 --}}
 
-<a href='/staffs/dashboard'>Dashboardaaa</a>
+<a href='/staffs/dashboard'>Dashboard Add new stuff</a>
 <a href="/staffs/students/viewStudent" class="btn btn-primary">Student</a>
 <a href="/students/create" class="btn btn-primary">Student</a>
